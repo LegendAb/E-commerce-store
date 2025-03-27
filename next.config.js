399672d98c2851fa75d9195dde9f2a079f7b6ca5
LegendAb <abraham.jonathan2015@gmail.com>
2025-03-27 23:30:24 +0100

    initial commit
    very first and initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        appDir: true,
+      },
+    env: {
+        API_URL: "http://localhost:3000",
+        DB_URI: "mongodb://localhost:27017/legendStore",
+        NEXTAUTH_SECRET: "legend",
+    },
+    images: {
+        domains: ["res.cloudinary.com"]
+    }
+}
+
+module.exports = nextConfig
---
+++ pages/api/products/index.js
@@ -0,0 +1,14 @@
+import nc from "next-connect";
+import dbConnect from "@/backend/config/dbConnect";
+import { getProducts, newProduct, } from "@/backend/controllers/productControllers";
+import onError from "@/backend/middlewares/errors";
+
+  
+const handler = nc({ onError });
+
+dbConnect();
+
+handler.get(getProducts);
+handler.post(newProduct);
+
+export default handler;
